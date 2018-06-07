ac781144b481dd677ddb55da2ed2c16b4640bff3
clamond <clamond.ng@digitalturbine.com>
2018-06-08 06:16:23 +1000

    Unit test.

---
+++ MortgageCalculator.UnitTests/MortgageTest.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using NUnit.Framework;
+using MortgageCalculator.Web;
+
+namespace MortgageCalculator.UnitTests
+{
+    [TestFixture]
+    public class MortgageTest
+    {
+        [Test]
+        public void TestView()
+        {
+            var controller = new HomeController();
+            var result = controller.Details(2) as ViewResult;
+            Assert.AreEqual("Index", result.ViewName);
+        }
+    }
+}
